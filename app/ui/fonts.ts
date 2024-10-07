2105421a92a13a9b9018912da2fc5fc84b0eeda0
HACKER <jorge.novo.rodriguez.93@gmail.com>
2024-10-07 16:44:53 +0200

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Montserrat, Lusitana } from 'next/font/google'
+
+export const monstserrat = Montserrat( { subsets: ['latin']})
+
+export const lusitana = Lusitana( { subsets: ['latin'], weight: ['400', '700']})
