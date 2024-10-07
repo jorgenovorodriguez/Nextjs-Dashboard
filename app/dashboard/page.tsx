a809f1d101ef1b6afce0c0b8ec080eaf7431057e
HACKER <jorge.novo.rodriguez.93@gmail.com>
2024-10-07 23:46:39 +0200

    Chapter 11

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,56 @@
-export default function Page() {
-    return <p> Dashboard Page: </p>;
+import { log } from 'console';
+import { fetchCardData, fetchLatestInvoices, fetchRevenue } from '../lib/data';
+import RevenueChart from '../ui/dashboard/revenue-chart';
+import { lusitana } from '../ui/fonts';
+import LatestInvoices from '../ui/dashboard/latest-invoices';
+import { Card } from '../ui/dashboard/cards';
+import { Suspense } from 'react';
+import { LatestInvoicesSkeleton, RevenueChartSkeleton } from '../ui/skeletons';
+
+export default async function Page() {
+    const latestInvoices = await fetchLatestInvoices();
+    const {
+        numberOfInvoices,
+        numberOfCustomers,
+        totalPaidInvoices,
+        totalPendingInvoices,
+    } = await fetchCardData();
+
+    return (
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+                Dashboard
+            </h1>
+            <div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-4'>
+                <Card
+                    title='Collected'
+                    value={totalPaidInvoices}
+                    type='collected'
+                />
+                <Card
+                    title='Pending'
+                    value={totalPendingInvoices}
+                    type='pending'
+                />
+                <Card
+                    title='Total Invoices'
+                    value={numberOfInvoices}
+                    type='invoices'
+                />
+                <Card
+                    title='Total Customers'
+                    value={numberOfCustomers}
+                    type='customers'
+                />
+            </div>
+            <div className='mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8'>
+                <Suspense fallback={<RevenueChartSkeleton />}>
+                    <RevenueChart />
+                </Suspense>
+                <Suspense fallback={<LatestInvoicesSkeleton />}>
+                    <LatestInvoices latestInvoices={latestInvoices} />
+                </Suspense>
+            </div>
+        </main>
+    );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -10,9 +10,6 @@ export default function RootLayout({
         <html lang='en'>
             <body className={`${monstserrat.className} antialiased`}>
                 {children}
-                <footer className='py-10 flex justify-center items-center'>
-                    © 2024
-                </footer>
             </body>
         </html>
     );
